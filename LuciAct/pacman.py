e2e5709457137beb8c9c8bca03f2d60dd5801f19
JosuAlejandroAl <A01732454@tec.mx>
2022-11-04 16:18:03 -0600

    declare wall

--- LuciAct/pacman.py
+++ LuciAct/pacman.py
@@ -4,31 +4,85 @@ from mesa.time import RandomActivation
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 
+
 class Ghost(Agent):
-    def __init__(self, model, pos):
-        super().__init__(model.next_id(), model)
+    def  __init__(self, model, pos):
+        super(). __init__(model.next_id(), model)
         self.pos = pos
+        self.matrix = [
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0],
+            [0,1,0,1,0,0,0,1,1,1,0,1,0,1,0,1,0],
+            [0,1,1,1,0,1,0,0,0,0,0,1,0,1,1,1,0],
+            [0,1,0,0,0,1,1,1,1,1,1,1,0,0,0,1,0],
+            [0,1,0,1,0,1,0,0,0,0,0,1,1,1,0,1,0],
+            [0,1,1,1,0,1,0,1,1,1,0,1,0,1,0,1,0],
+            [0,1,0,1,0,1,0,1,1,1,0,1,0,1,0,1,0],
+            [0,1,0,1,1,1,0,0,1,0,0,1,0,1,1,1,0],
+            [0,1,0,0,0,1,1,1,1,1,1,1,0,0,0,1,0],
+            [0,1,1,1,0,1,0,0,0,0,0,1,0,1,1,1,0],
+            [0,1,0,1,0,1,0,1,1,1,0,0,0,1,0,1,0],
+            [0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            ]
     def step(self):
         next_moves = self.model.grid.get_neighborhood(self.pos, moore=False)
         next_move = self.random.choice(next_moves)
-        self.model.grid.move_agent(self, next_move)
-    
+        if(int(self.matrix[int(next_move[1])][int(next_move[0])])==1):
+            self.model.grid.move_agent(self, next_move)
+
+
+
+class WallBlock(Agent):
+    def  __init__(self, model, pos):
+        super(). __init__(model.next_id(), model)
+        self.pos = pos
+
+
 
 class Maze(Model):
-    def __init__(self):
-        super().__init__()
+    def  __init__(self):
+        super(). __init__()
         self.schedule = RandomActivation(self)
         self.grid = Grid(17, 14, torus=False)
+        matrix = [
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0],
+            [0,1,0,1,0,0,0,1,1,1,0,1,0,1,0,1,0],
+            [0,1,1,1,0,1,0,0,0,0,0,1,0,1,1,1,0],
+            [0,1,0,0,0,1,1,1,1,1,1,1,0,0,0,1,0],
+            [0,1,0,1,0,1,0,0,0,0,0,1,1,1,0,1,0],
+            [0,1,1,1,0,1,0,1,1,1,0,1,0,1,0,1,0],
+            [0,1,0,1,0,1,0,1,1,1,0,1,0,1,0,1,0],
+            [0,1,0,1,1,1,0,0,1,0,0,1,0,1,1,1,0],
+            [0,1,0,0,0,1,1,1,1,1,1,1,0,0,0,1,0],
+            [0,1,1,1,0,1,0,0,0,0,0,1,0,1,1,1,0],
+            [0,1,0,1,0,1,0,1,1,1,0,0,0,1,0,1,0],
+            [0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            ]
 
         ghost = Ghost(self, (8, 6))
         self.grid.place_agent(ghost, ghost.pos)
         self.schedule.add(ghost)
+
+        for _,x,y in self.grid.coord_iter():
+            if matrix[y][x] == 0:
+                print(f"block at ({x},{y})")
+                wall = WallBlock(self, (x,y))
+                self.grid.place_agent(wall, wall.pos)
+                self.schedule.add(wall)
         
     def step(self):
         self.schedule.step()
 
 def agent_portrayal(agent):
-  return {"Shape": "ghost.png", "Layer": 0}
+    if (type(agent)==Ghost):
+        return {"Shape": "ghost.png", "Layer": 0}
+    else:
+        if(type(agent)==WallBlock):
+            return {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Color": "Gray", "Layer": 0}
+        
 
 grid = CanvasGrid(agent_portrayal, 17, 14, 450, 450)
 
